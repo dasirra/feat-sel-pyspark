4f1fc1804e6e0e3396c17ce67adccea0dccccd56
Daniel <dasirra@gmail.com>
2016-06-12 19:17:49 +0200

    Add univariate functions

---
+++ feat_sel/mathutils.py
@@ -0,0 +1,30 @@
+
+from pyspark.mllib.stat import Statistics
+
+def dist_corr(v1, v2):
+
+	return Statistics.corr(v1,v2)
+
+def dist_ftest(v, t):
+    
+    # calculate auxiliary variables
+    n_samples = v.count()
+    n_groups = t.distinct().count() # number of distinct groups
+    overall_mean = vector.mean() # overall mean
+    aux_mean = z_vector.aggregateByKey((0,0),
+                                  lambda x,y: (x[0]+y,x[1]+1),
+                                  lambda x,y: (x[0]+y[0],x[1]+y[1]))
+    group_count = aux_mean.map(lambda (label,x): (label,x[1])) 
+    group_mean = aux_mean.map(lambda (label,x): (label,x[0]/x[1])) 
+    aux_within = z_vector.leftOuterJoin(group_mean)
+
+    # between-group variability
+    num = sum([nx[1]*(mx[1]-overall_mean)**2
+    	for (nx,mx) in zip(group_count.collect(),
+    		group_mean.collect())])/float(n_groups-1)
+    
+    # within-group variability
+    den = aux_within.map(lambda (_,x): (x[0]-x[1])**2) \
+    	.reduce(add)/float(n_samples-n_groups)
+    
+    return num/den
---
+++ feat_sel/univariate.py
@@ -0,0 +1,56 @@
+from operator import add
+
+from pyspark.ml.feature import RFormula
+
+from mathutils import *
+
+class SelectKBest():
+
+	def __init__(self, k=3, method="corr"):
+
+		self.k_ = k
+
+		if method == 'corr':
+			self.sfunc_ = dist_corr
+		elif method == 'fscore':
+			self.sfunc_ = dist_ftest
+		else:
+			raise ValueError('Invalid method: only corr or fscore')
+
+		self._fitted = False
+
+	def fit(self, df, featureCols, targetCol):
+
+		# build features assemble
+		formula = RFormula(
+		    formula = '{target} ~ + {predictors}.'format(target=targetCol,
+		    	predictors='+'.join(featureCols)),
+		    featuresCol = 'features',
+		    labelCol = 'target'
+		)
+		assembled_df = formula.fit(df).transform(df)
+
+		# extract features and target
+		feats = out_df.select('features').rdd
+		feats = feats.map(lambda x: x['features'])
+		target = out_df.select('target').rdd
+		target = target.map(lambda x: x['target'])
+
+		# compute per-column metric
+		scores = []
+		for i,feat in enumerate(featureCols):
+			vector = feats.map(lambda x: x[i])
+			scores.append(self.sfunc_(vector,target))
+		self.scores_ = scores
+
+		self._fitted = True
+
+	def transform(self, df):
+
+		if not self._fitted:
+			raise RuntimeError('First call fit() method')
+
+		cols = df.columns
+		idx = sorted(range(len(self.scores_)),key=self.scores_.__getitem__)
+
+		return df.select(cols[idx])
